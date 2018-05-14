4b605513508b6e4c0b52d254b69a8c4c0d994653
BOJOY\yaohao <403666963@qq.com>
2018-05-14 08:50:53 +0800

    bug

--- ch1/classpath/entry.go
+++ ch1/classpath/entry.go
@@ -1,6 +1,9 @@
 package classpath
 
-import "os"
+import (
+	"os"
+	"strings"
+)
 
 const pathListSeparator = string(os.PathListSeparator)
 
@@ -10,5 +13,18 @@ type Entry interface {
 }
 
 func newEntry(path string) Entry {
+	if strings.Contains(path, pathListSeparator) {
+		return newCompositeEntry(path)
+	}
 
+	if strings.HasSuffix(path, "*") {
+		return newWildcardEntry(path)
+	}
+
+	if strings.HasSuffix(path, ".jar") || strings.HasSuffix(path, ".JAR") ||
+		strings.HasSuffix(path, ".zip") || strings.HasSuffix(path, ".ZIP") {
+
+		return newZipEntry(path)
+	}
+	return newDirEntry(path)
 }
